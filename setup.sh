508fa5e2a242ec74406340c72e375043643962b9
chapochapo <xl4k@pm.me>
2026-01-09 19:14:25 +0100

    feat: setup.sh + .zshrc

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "This script is intended to be used on archlinux (btw)"
+sudo pacman -S \
+  git \
+  signal-desktop \
+  wget curl \
+  vlc vlc-plugins-all \
+  zsh zsh-autosuggestions zsh-completions zsh-syntax-highlighting
